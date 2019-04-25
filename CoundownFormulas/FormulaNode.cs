79b575c4d3a69c50b697350ddfdd23153ed8e94b
BlockWorker <alex.kharitonov.2000@gmail.com>
2019-04-25 14:54:19 +0200

    Initial Commit

---
+++ CoundownFormulas/FormulaNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoundownFormulas {
+    public class FormulaNode {
+        public List<FormulaNode> Roots = new List<FormulaNode>();
+        public HashSet<int> Values = new HashSet<int>();
+
+        public FormulaNode(int number) {
+            Roots.Add(this);
+            Values.Add(number);
+        }
+
+        public FormulaNode(FormulaNode p1, FormulaNode p2) {
+            if (p1 & p2) throw new ArgumentException("Parents cannot share roots");
+
+            Roots.AddRange(p1.Roots);
+            Roots.AddRange(p2.Roots);
+
+            int v;
+            foreach (int a in p1.Values) {
+                foreach (int b in p2.Values) {
+                    v = a + b;
+                    if (v <= 999) Values.Add(v);
+                    v = a - b;
+                    if (v >= 0) Values.Add(v);
+                    v = b - a;
+                    if (v >= 0) Values.Add(v);
+                    v = a * b;
+                    if (v <= 999) Values.Add(v);
+                    if (b > 1 && a % b == 0) Values.Add(a / b);
+                    if (a > 1 && b % a == 0) Values.Add(b / a);
+                }
+            }
+        }
+
+        public static bool operator &(FormulaNode a, FormulaNode b) {
+            return a.Roots.Intersect(b.Roots).Count() > 0;
+        }
+
+        public override string ToString() {
+            string s = "[ ";
+            foreach (int v in Values) s += v + " ";
+            return s + "]";
+        }
+    }
+}
---
+++ CoundownFormulas/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoundownFormulas {
+    public class Program {
+        public static void Main(string[] args) {
+            var numbers = new int[] { 3, 12, 30, 56, 60, 90 };
+            GetTree(numbers);
+        }
+
+        public static List<HashSet<int>> GetTree(int[] numbers) {
+            var tree = new List<List<FormulaNode>>();
+            var level = new List<FormulaNode>();
+
+            //Level 0: Just the numbers
+            foreach (int n in numbers) level.Add(new FormulaNode(n));
+            tree.Add(level);
+
+            for (int nLevel = 1; nLevel < numbers.Length; nLevel++) {
+                level = new List<FormulaNode>();
+                int iterations = nLevel % 2 == 0 ? nLevel / 2 : nLevel / 2 + 1;
+
+                for (int i = 1; i <= iterations; i++) {
+                    if (i - 1 == nLevel / 2) {
+                        var parentLevel = tree[i - 1];
+                        for (int j = 0; j < parentLevel.Count - 1; j++) {
+                            var n1 = parentLevel[j];
+                            for (int k = 1; k < parentLevel.Count; k++) {
+                                var n2 = parentLevel[k];
+                                if (n1 & n2) continue;
+                                level.Add(new FormulaNode(n1, n2));
+                            }
+                        }
+                    } else {
+                        var parentLevel1 = tree[nLevel - i];
+                        var parentLevel2 = tree[i - 1];
+                        for (int j = 0; j < parentLevel1.Count; j++) {
+                            var n1 = parentLevel1[j];
+                            for (int k = 0; k < parentLevel2.Count; k++) {
+                                var n2 = parentLevel2[k];
+                                if (n1 & n2) continue;
+                                level.Add(new FormulaNode(n1, n2));
+                            }
+                        }
+                    }
+                }
+
+                tree.Add(level);
+            }
+
+            var ret = new List<HashSet<int>>();
+            foreach (var lv in tree) {
+                var set = new HashSet<int>();
+                foreach (var node in lv) {
+                    foreach (int v in node.Values) set.Add(v);
+                }
+                ret.Add(set);
+            }
+
+            return ret;
+        }
+    }
+}
